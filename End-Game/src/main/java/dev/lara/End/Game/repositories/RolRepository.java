fde74d1c880f5463a06b2ba7b96bd118dce23ff8
lara-gs <lara_989@hotmail.com>
2024-11-23 17:52:13 +0100

    repositorio de rol hecho

---
+++ End-Game/src/main/java/dev/lara/End/Game/repositories/RolRepository.java
@@ -0,0 +1,9 @@
+package dev.lara.End.Game.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.lara.End.Game.models.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+}
