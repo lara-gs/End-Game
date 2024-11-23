4d643304ebd916c9cec2aa5636a0da20ddbe731a
lara-gs <lara_989@hotmail.com>
2024-11-23 18:08:56 +0100

    creados el resto de repositorios

--- End-Game/src/main/java/dev/lara/End/Game/repositories/Rolepository.java
+++ End-Game/src/main/java/dev/lara/End/Game/repositories/Rolepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.lara.End.Game.models.Rol;
 
-public interface RolRepository extends JpaRepository<Rol, Integer> {
+public interface Rolepository extends JpaRepository<Rol, Integer> {
 
 }
