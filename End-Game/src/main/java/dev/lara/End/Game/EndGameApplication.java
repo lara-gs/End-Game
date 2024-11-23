eebbdeb4aebccf87e4a96b9daf38e48aeb4d8896
lara-gs <lara_989@hotmail.com>
2024-11-23 17:03:00 +0100

    creacion del proyecto

---
+++ End-Game/src/main/java/dev/lara/End/Game/EndGameApplication.java
@@ -0,0 +1,13 @@
+package dev.lara.End.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndGameApplication.class, args);
+	}
+
+}
